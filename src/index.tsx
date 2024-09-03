ef3e05de67b12c6917359363dcbbb8c64a8f610d
WillDotWhite <willdotwhiteis@gmail.com>
2024-09-03 18:19:43 +0100

    Hexclock lol

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,28 @@ import { LocationProvider, Router, Route, hydrate, prerender as ssr } from 'prea
 import { Home } from './pages/Home';
 import { NotFound } from './pages/_404.jsx';
 import './style.css';
+import {useEffect, useState} from 'react';
 
 export function App() {
+	const [timeAsHex, setTimeAsHex] = useState(getTimeAsHex())
+	useEffect(() => {
+		setInterval(() => setTimeAsHex(getTimeAsHex()), 1000)
+	}, []);
+
+	function getTimeAsHex() {
+		return `#${new Date().toLocaleTimeString().replaceAll(":", "")}`;
+	}
+
+	console.log(timeAsHex)
+
 	return (
 		<LocationProvider>
-			<main>
+			<main style={{backgroundColor: `${timeAsHex}`}}>
 				<Router>
 					<Route path="/" component={Home} />
 					<Route default component={NotFound} />
 				</Router>
+				<p style={{fontSize: `0.75rem`}}>The current time colour is: {timeAsHex}</p>
 			</main>
 		</LocationProvider>
 	);
